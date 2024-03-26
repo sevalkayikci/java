b845e27911e353de98a4e8c58dfad3567cc965ae
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-27 01:39:02 +0200

    if else example

---
+++ ifelse_example.java
@@ -0,0 +1,12 @@
+public class ifelse_example {
+    public static void main(String[] args) {
+        int grade = 87;
+
+        if (grade < 50) {
+            System.out.println("you failed");
+        }
+        else{
+            System.out.println("you passed");
+        }
+    }
+}
