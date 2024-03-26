7809a47948a70e368418910b79540b622c28218d
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-27 01:35:20 +0200

    if example

---
+++ ifexample.java
@@ -0,0 +1,15 @@
+public class ifexample {
+    public static void main(String[] args) {
+
+        int age = 16;
+
+        if (age < 18) {
+
+            System.out.println("age is under 18");
+
+            
+        }
+    }
+
+    
+}
