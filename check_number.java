4081222796c8df2f1f8477b242f4efed12b84904
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-27 01:38:42 +0200

    check numbers using if statement

---
+++ check_number.java
@@ -0,0 +1,17 @@
+public class check_number {
+    public static void main(String[] args) {
+        
+        int number = -9;
+
+        if (number < 0) {
+            System.out.println("number is negative");
+        }
+        else if (number > 0) {
+            System.out.println("number is positive");
+            
+        }
+        else{
+            System.out.println("number is zero");
+        }
+    }
+}
