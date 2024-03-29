3b15b73e4b89e2377518800af402a81987fa2953
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-29 11:59:51 +0200

    start and end func

---
+++ startend.java
@@ -0,0 +1,18 @@
+public class startend {
+    public static void main(String[] args) {
+        
+        String s = "Hello seval";
+
+        System.out.println(s.startsWith("H"));
+        System.out.println(s.startsWith("h"));
+        System.out.println(s.startsWith("Hello"));
+
+        System.out.println(s.endsWith("al"));
+        System.out.println(s.endsWith("Seval"));
+
+
+
+
+
+    }
+}
