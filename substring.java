c5f0ee08529b4683be34b08027e225c128376c22
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-29 11:59:06 +0200

    substrings

---
+++ substring.java
@@ -0,0 +1,10 @@
+public class substring {
+    public static void main(String args[]){    
+        String s="Hello seval"; 
+
+        System.out.println("Original String: " + s);  
+        System.out.println("Substring starting from index 6: " +s.substring(6));
+        System.out.println("Substring starting from index 0 to 6: "+s.substring(0,6)); 
+    }  
+    
+}
