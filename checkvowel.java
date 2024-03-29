164f2cac681d9889c96d6a9b6853aa61a8f8e486
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-29 11:56:51 +0200

    checks vowels

---
+++ checkvowel.java
@@ -0,0 +1,51 @@
+public class checkvowel {
+    public static void main(String[] args) {
+        
+        char ch = 'S';
+
+        switch (ch) {
+            case 'a': 
+            System.out.println("vowel");
+                break;
+
+            case 'e':
+            System.out.println("vowel");
+                break;
+            
+            case 'i': 
+            System.out.println("vowel");
+                break;
+
+            case 'o':
+            System.out.println("vowel");
+                break;
+            
+            case 'u': 
+                System.out.println("vowel");
+                break;
+    
+            case 'A':
+                System.out.println("vowel");
+                break;
+
+            case 'E': 
+                System.out.println("vowel");
+                break;
+    
+            case 'I':
+                System.out.println("vowel");
+                break; 
+            
+            case 'O': 
+                System.out.println("vowel");
+                    break;
+    
+            case 'U':
+                System.out.println("vowel");
+                break;
+            default:
+                 System.out.println("consonant");
+                break;
+        }
+    }
+}
