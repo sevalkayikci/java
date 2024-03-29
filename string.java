9952f253011c8e465dcdb1080a10585eb35dfbd4
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-29 11:57:54 +0200

    string

---
+++ string.java
@@ -0,0 +1,10 @@
+public class string {
+    public static void main(String[] args) {
+        String s1 = "hello";
+        char ch[] = {'s', 'e', 'v', 'a', 'l'};
+        String s2 =  new String(ch); //convert char to string
+
+        System.out.println(s1);
+        System.out.println(s2);
+    }
+}
