c46baa0cbbe7a8a9e7f9ec2ee4468425dbd2d06e
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-29 11:58:54 +0200

    concatenation

---
+++ concatenation.java
@@ -0,0 +1,15 @@
+public class concatenation {
+    public static void main(String[] args) {
+        String s = "seval" + " " + "kayikci";
+        
+        String s1 = "hello";
+        String s2 = "Seval";
+
+        String s3 = s1.concat(s2);
+
+        System.out.println(s);
+        System.out.println(s3);
+
+         
+    }
+}
