4e893c0dac15dc7b32348d024c8b4f6993013644
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-29 11:56:26 +0200

    switch case

---
+++ switchexample.java
@@ -0,0 +1,21 @@
+public class switchexample {
+    public static void main(String[] args) {
+        int number = 20;
+
+        switch (number) {
+            case 10:
+                System.out.println("10");
+                break;
+            
+            case 20:
+                System.out.println("20");
+                break;   
+        
+            default:
+            System.out.println("error");
+                break;
+        }
+
+
+    }
+}
