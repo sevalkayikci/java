b469c7daaab61678ba6adb60e1580f3833622b9d
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-29 12:00:02 +0200

    operations

---
+++ operations.java
@@ -0,0 +1,11 @@
+public class operations {
+    public static void main(String[] args) {
+        String s = "hello seval";
+
+        System.out.println(s.charAt(0));
+
+        System.out.println(s.charAt(8));
+
+        System.out.println(s.length());
+    }
+}
