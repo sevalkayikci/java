1c3920678f69e4bd776fd30b5d0ad4705aef13d2
sevalkayikci <66740624+sevalkayikci@users.noreply.github.com>
2024-03-29 11:59:26 +0200

    uppercase lowercase func

---
+++ upperlowecase.java
@@ -0,0 +1,9 @@
+public class upperlowecase {
+    public static void main(String[] args) {
+        String s = "seval";
+        String s2 = "ZeYtin";
+
+        System.out.println(s.toUpperCase());
+        System.out.println(s2.toLowerCase());
+    }
+}
